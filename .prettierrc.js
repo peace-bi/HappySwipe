2a57c49858087ba1551fca98d3f2b4ebae491478
Bi <linhnguyen93x@gmail.com>
2019-12-08 22:36:45 +0700

    first template

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: "none",
+  arrowParens: "always",
+  semi: false
+};
